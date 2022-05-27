00d601448d81f2b9c4266365cebf5b2ac3722288
rox <roxmarzan14@gmail.com>
2022-05-27 22:33:08 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB1SLcm0m_p1VGeR4zR52kTaxhmY1FiBvI",
+    authDomain: "art-gallery-1314f.firebaseapp.com",
+    projectId: "art-gallery-1314f",
+    storageBucket: "art-gallery-1314f.appspot.com",
+    messagingSenderId: "283025157331",
+    appId: "1:283025157331:web:d2cd4ac29025388ac8b826"
+};
+
+initializeApp(firebaseConfig)
